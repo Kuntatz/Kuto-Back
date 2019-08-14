2748da2d8f547b4a76a81351b1b6c058941881ee
Rati Kirvalidze <rati.kirvalidze10@gmail.com>
2019-08-14 18:58:06 +0400

    implemented delete/update/get/get all apis

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -18,23 +18,69 @@ export const verifyToken = async (req, res, next) => {
 }
 
 export const signup = async (req, res) => {
-  const { email, password } = req.body;
-  console.info('admin', admin);
+  const { email, password, username } = req.body;
   try {
     const user = await admin.auth().createUser({
       email,
       password,
       disabled: false,
       emailVerified: true,
-      displayName: 'John Doe'
+      displayName: username
     });
-    return res.send({
-      user
-    });
-  } catch(e) {
+    return res.send({ user: user.toJSON() });
+  } catch (e) {
     console.info('error signup', e);
-    return res.send({
-      error: e
-    });
+    return res.status(httpStatus.BAD_REQUEST).send({ error: e });
+  }
+}
+
+export const deleteUser = async (req, res) => {
+  try {
+    const result = await admin.auth().deleteUser(req.params.uid);
+    return res.send({ success: true, msg: 'Successfully deleted user' });
+  } catch (e) {
+    return res.status(httpStatus.BAD_REQUEST).send({ error: e });
+  }
+}
+
+export const updateUser = async (req, res) => {
+  const { email, password, username } = req.body;
+  const updatedUser = {};
+  if (email) {
+    updatedUser.email = email;
+  }
+  if (password) {
+    updatedUser.password = password;
+  }
+  if (username) {
+    updatedUser.displayName = username;
+  }
+  try {
+    const user = await admin.auth().updateUser(req.params.uid, updatedUser);
+    return res.send({ user: user.toJSON() });
+  } catch (e) {
+    return res.status(httpStatus.BAD_REQUEST).send({ error: e });
+  }
+}
+
+export const getUser = async (req, res) => {
+  try {
+    const userRecord = await admin.auth().getUser(req.params.uid);
+    return res.send({ user: userRecord.toJSON() });
+  } catch (e) {
+    return res.status(httpStatus.BAD_REQUEST).send({ error: e });
+  }
+}
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = [];
+    const result = await admin.auth().listUsers(1000, req.params.nextPageToken);
+    for (let i = 0; i < result.users.length; i += 1) {
+      users.push(result.users[i].toJSON());
+    }
+    return res.send({ users, nextPageToken: result.pageToken });
+  } catch (e) {
+    return res.status(httpStatus.BAD_REQUEST).send({ error: e });
   }
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,7 +5,14 @@ import paramValidation from '../config/param-validation';
 
 const router = express.Router();
 
-router.route('/signup')
-  .post(validate(paramValidation.createUser), authCtrl.signup);
+router.route('/signup').post(validate(paramValidation.createUser), authCtrl.signup);
+
+router.route('/delete/:uid').delete(authCtrl.deleteUser);
+
+router.route('/update/:uid').put(authCtrl.updateUser);
+
+router.route('/:uid').get(authCtrl.getUser);
+
+router.route('/all/:nextPageToken').get(authCtrl.getAllUsers);
 
 export default router;
