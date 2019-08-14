7c61e84a7c88fae6961aa64ebc7a9b6e13832542
Rati Kirvalidze <rati.kirvalidze10@gmail.com>
2019-08-14 18:56:54 +0400

    updated createUser param validation

--- src/config/param-validation.js
+++ src/config/param-validation.js
@@ -4,7 +4,8 @@ export default {
   createUser: {
     body: {
       email: Joi.string().email().required(),
-      password: Joi.string().required()
+      password: Joi.string().required(),
+      username: Joi.string().alphanum().required()
     }
   }
 }
